039e1a5dbdbc41b538087200d876d935adc72b14
pentinmaki <keijo.pentinmaki@gmail.com>
2025-01-11 18:54:42 +0200

    lisää useState-muuttujan napautuksille

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { useState } from 'react';
 import Lemon from './components/Lemon';
 import Balance from './components/Balance';
 import Booster from './components/Booster';
@@ -6,13 +7,22 @@ import Header from './components/Header';
 
 
 function App() {
+    // Luodaan tilamuuttuja, jossa tallennetaan napautusten määrä.
+    const [clicks, setClicks] = useState(0);
+
+    const handleClick = () => {
+      // Kasvatetaan napautusten määrää yhdellä.
+      setClicks(clicks + 1);
+    }
+  
+
 
   return (
     <>
       <div>
         <Header>lemon clicker</Header>
-        <Balance total="157" />
-        <Lemon />
+        <Lemon onClick={handleClick} /> 
+        <Balance total={clicks} />
         <Booster value="3.2" />
       </div>  
     </>
